16c30bd9d0ead1353961e0671afed8ab1983fc3a
Dmitry Tishkovsky <git@dmitry.plus.com>
2024-01-17 19:11:43 +0000

    Initial implementation, no rules

--- cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -1,24 +1,47 @@
 package uk.gov.dwp.uc.pairtest;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TicketServiceImplTest {
 
+    @InjectMocks
+    private TicketServiceImpl ticketService;
+
+    @Mock
+    private TicketPaymentService ticketPaymentService;
+
+    @Mock
+    private SeatReservationService seatReservationService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void validateRequestLimit() {
-        TicketService ticketService = new TicketServiceImpl();
-        Long accountId = Long.valueOf(0);
+        Long accountId = 0L;
         try {
-            TicketTypeRequest[] requests = new TicketTypeRequest[21];
+            TicketTypeRequest[] requests = new TicketTypeRequest[TicketServiceImpl.MAX_REQUESTS + 1];
             ticketService.purchaseTickets(accountId, requests);
-            assertTrue("InvalidPurchaseException is not thrown when request limit is exceeded", false);
+            fail(String.format(
+                    "InvalidPurchaseException is not thrown when request limit %d is exceeded",
+                    TicketServiceImpl.MAX_REQUESTS));
         } catch (InvalidPurchaseException e) {
             assertTrue(true);
         }
