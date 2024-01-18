31faac161b737dfc77a36bdfde89c49eec8f9293
Dmitry Tishkovsky <git@dmitry.plus.com>
2024-01-18 21:04:18 +0000

    Closes #8

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -3,9 +3,8 @@ package uk.gov.dwp.uc.pairtest;
 import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
-import uk.gov.dwp.uc.pairtest.exception.*;
-
-import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.ADULT;
+import uk.gov.dwp.uc.pairtest.exception.InvalidAccountException;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
 
