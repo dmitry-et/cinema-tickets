5aa18b3a2cdeed46cc028cbb13d09bb1b5239659
Dmitry Tishkovsky <git@dmitry.plus.com>
2024-01-18 13:41:24 +0000

    TicketService is implemeted, more tests required

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -3,10 +3,9 @@ package uk.gov.dwp.uc.pairtest;
 import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
-import uk.gov.dwp.uc.pairtest.exception.InvalidAccountException;
-import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
-import uk.gov.dwp.uc.pairtest.exception.InvalidRequestException;
-import uk.gov.dwp.uc.pairtest.exception.TooManyTicketsException;
+import uk.gov.dwp.uc.pairtest.exception.*;
+
+import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.ADULT;
 
 public class TicketServiceImpl implements TicketService {
 
@@ -31,20 +30,38 @@ public class TicketServiceImpl implements TicketService {
             throws InvalidPurchaseException {
 
         verify(accountId);
-        verify(ticketTypeRequests);
 
         int totalAmountToPay = 0;
-        ticketPaymentService.makePayment(accountId, totalAmountToPay);
         int totalSeatsToAllocate = 0;
+        int totalTickets = 0;
+        boolean adult = false;
+        for(TicketTypeRequest ticketTypeRequest: ticketTypeRequests) {
+            if(ticketTypeRequest == null) throw new InvalidRequestException(); //TODO: Consider a new exception class
+            int noOfTickets = ticketTypeRequest.getNoOfTickets();
+            if(noOfTickets <= 0) throw new InvalidRequestException(); //TODO: Consider a new exception class
+            totalTickets += noOfTickets;
+            if(totalTickets > MAX_REQUESTS) throw new TooManyTicketsException();
+            totalSeatsToAllocate += noOfTickets;
+            TicketTypeRequest.Type ticketType = ticketTypeRequest.getTicketType();
+            adult = adult || (ADULT == ticketType);
+            int price = getPricePerTicket(ticketType);
+            totalAmountToPay += (price * noOfTickets);
+        }
+        if(!adult) throw new NoAdultTicketsException();
+        ticketPaymentService.makePayment(accountId, totalAmountToPay);
         seatReservationService.reserveSeat(accountId, totalSeatsToAllocate);
     }
 
+    private int getPricePerTicket(TicketTypeRequest.Type ticketType) { //TODO: Consider float type for a price
+        switch(ticketType) {
+            case ADULT: return 20;
+            case CHILD: return 10;
+            case INFANT: return 0;
+            default: throw new UnknownTicketTypeException();
+        }
+    }
+
     private void verify(Long accountId) throws InvalidAccountException {
-        if(accountId <= 0) throw new InvalidAccountException();
+        if(accountId == null || accountId <= 0) throw new InvalidAccountException();
     }
-
-    private void verify(TicketTypeRequest[] ticketTypeRequests)throws InvalidRequestException {
-        if(ticketTypeRequests.length > MAX_REQUESTS) throw new TooManyTicketsException();
-    }
-
 }
--- cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -10,6 +10,7 @@ import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidAccountException;
+import uk.gov.dwp.uc.pairtest.exception.InvalidRequestException;
 import uk.gov.dwp.uc.pairtest.exception.TooManyTicketsException;
 
 import static org.junit.Assert.fail;
@@ -33,6 +34,18 @@ public class TicketServiceImplTest {
     }
 
     @Test
+    public void nullAccount() {
+        final Long invalidAccountId = null;
+        try {
+            ticketService.purchaseTickets(invalidAccountId);
+            fail(String.format(
+                    "InvalidAccountException is not thrown for the account id %d is not greater",
+                    invalidAccountId));
+        } catch (InvalidAccountException ignored) {
+        }
+    }
+
+    @Test
     public void invalidAccount() {
         final Long invalidAccountId = 0L;
         try {
@@ -48,6 +61,9 @@ public class TicketServiceImplTest {
     public void invalidRequestLimit() {
         final Long accountId = 1L;
         final TicketTypeRequest[] requests = new TicketTypeRequest[TicketServiceImpl.MAX_REQUESTS + 1];
+        for(int i = 0; i < requests.length; i++) {
+            requests[i] = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+        }
         try {
             ticketService.purchaseTickets(accountId, requests);
             fail(String.format(
@@ -56,4 +72,44 @@ public class TicketServiceImplTest {
         } catch (TooManyTicketsException ignored) {
         }
     }
+
+    @Test
+    public void invalidRequestLimit0() {
+        final Long accountId = 1L;
+        final TicketTypeRequest[] requests = new TicketTypeRequest[]{
+            new TicketTypeRequest(TicketTypeRequest.Type.INFANT, TicketServiceImpl.MAX_REQUESTS + 1)
+        };
+        try {
+            ticketService.purchaseTickets(accountId, requests);
+            fail(String.format(
+                    "InvalidPurchaseException is not thrown when request limit %d is exceeded",
+                    TicketServiceImpl.MAX_REQUESTS));
+        } catch (TooManyTicketsException ignored) {
+        }
+    }
+
+    @Test
+    public void nullRequest() {
+        final Long accountId = 1L;
+        final TicketTypeRequest[] requests = new TicketTypeRequest[]{ null };
+        try {
+            ticketService.purchaseTickets(accountId, requests);
+            fail("InvalidRequestException is not thrown when request is null");
+        } catch (InvalidRequestException ignored) {
+        }
+    }
+
+    @Test
+    public void noOfTicketsIsNotPositive() {
+        final Long accountId = 1L;
+        final TicketTypeRequest[] requests = new TicketTypeRequest[]{
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 0)
+        };
+        try {
+            ticketService.purchaseTickets(accountId, requests);
+            fail("InvalidRequestException is not thrown when no of ticket in the request is 0");
+        } catch (InvalidRequestException ignored) {
+        }
+    }
+
 }
